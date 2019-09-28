013eedbb7f98161f6a81178df5d35daaee759ea7
Ricardo <ricardo.augusto.cardona@gmail.com>
2019-09-27 21:19:14 -0300

    Inclusão de botão e de UseEffect para bindar imc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
-import {ScrollView, View, Text, TextInput} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ScrollView, View, Text, TextInput, Button, Alert} from 'react-native';
 
 const App = () => {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
-  //const [imc, setImc] = useState('');
+  const [imc, setImc] = useState('');
+
+  useEffect(() => {
+    if (peso !== '' && altura !== '') {
+      const value = parseFloat(peso) / Math.pow(parseFloat(altura / 100), 2);
+      setImc(value.toFixed(2));
+    }
+  }, [peso, altura, imc]);
+
+  const calculaImc = () => {
+    return parseFloat(peso) / Math.pow(parseFloat(altura / 100), 2);
+  };
 
   const onChangePesoHandler = input => {
     setPeso(input);
@@ -30,34 +41,55 @@ const App = () => {
     );
   };
 
-  const calculaImc = () => {
-    const imc = peso / Math.pow(altura / 100, 2);
-    return imc.toFixed(2);
+  const testaImc = () => {
+    var tipoImc = '';
+    if (!imc) {
+      tipoImc = 'Sem valor';
+    } else if (imc < 17) {
+      tipoImc = 'Muito abaixo do peso';
+    } else if (imc < 18.5) {
+      tipoImc = 'Abaixo do peso';
+    } else if (imc < 25) {
+      tipoImc = 'Peso normal';
+    } else if (imc < 30) {
+      tipoImc = 'Acima do peso';
+    } else if (imc < 35) {
+      tipoImc = 'Obesidade I';
+    } else if (imc < 40) {
+      tipoImc = 'Obesidade II (severa)';
+    } else {
+      tipoImc = 'Obesidade III (mórbida)';
+    }
+    return tipoImc;
   };
 
-  // const testaImc = () => {
-    
-  // }
+  const verificaImcHandler = () => {
+    setPeso('');
+    setAltura('');
+    Alert.alert('Resultado', `IMC: ${imc}, ${testaImc()}`);
+    setImc('');
+  };
 
   return (
     <ScrollView>
+      {exibirPeso()}
       <TextInput
         value={peso}
         keyboardType="numeric"
         placeholder="Digite o peso"
         onChangeText={onChangePesoHandler}
       />
-      {exibirPeso()}
 
+      {exibirAltura()}
       <TextInput
         value={altura}
         keyboardType="numeric"
         placeholder="Digite a altura em centimetros"
         onChangeText={onChangeAlturaHandler}
       />
-      {exibirAltura()}
+
       <View>
-        <Text>IMC: {calculaImc()}</Text>
+        <Button title="Verificar IMC" onPress={() => verificaImcHandler()} />
       </View>
     </ScrollView>
   );
